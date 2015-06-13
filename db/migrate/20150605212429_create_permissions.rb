08ca1f6d6a57b072540271c71062a1eb7e834ce7
Sherif Gamal <sherif.gamal.1990@gmail.com>
2015-06-13 09:42:04 +0200

    Finalized

--- db/migrate/20150605212429_create_permissions.rb
+++ db/migrate/20150605212429_create_permissions.rb
@@ -1,7 +1,7 @@
 class CreatePermissions < ActiveRecord::Migration
   def change
     create_table :permissions do |t|
-      t.string :transaction_type
+      t.integer :transaction_type
        # 1 = add material, 2 = release material, 3 = add product, 4 release product
        # 5 = release money for material, 6 = release money for expense
       t.integer :transaction_id
