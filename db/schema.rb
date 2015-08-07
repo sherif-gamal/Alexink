0144bc71a4e1bf62799040d03a122f2ae0545010
Sherif Gamal <sherif.gamal.1990@gmail.com>
2015-08-07 17:19:46 +0200

    minors

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150806213037) do
+ActiveRecord::Schema.define(version: 20150807144332) do
 
   create_table "add_material_permissions", force: :cascade do |t|
     t.integer  "transaction_id"
@@ -410,6 +410,11 @@ ActiveRecord::Schema.define(version: 20150806213037) do
     t.integer  "quantity"
     t.datetime "created_at",     null: false
     t.datetime "updated_at",     null: false
+  end
+
+  create_table "revisions", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "suppliers", force: :cascade do |t|
