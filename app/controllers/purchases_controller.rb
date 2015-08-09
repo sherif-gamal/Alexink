7f286c13e4bebe1b1d3b4f79e4d1e4f1deb374c5
Sherif Gamal <sherif.gamal.1990@gmail.com>
2015-08-09 18:11:14 +0200

    minor

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -141,6 +141,7 @@ class PurchasesController < ApplicationController
   def destroy
     permissions = ReleaseProductPermission.where(transaction_id: @purchase.id)
     PurchasePaymentDetail.find(@purchase.id).destroy
+    Invoice.delete(@purchase.id)
     update_treasury(@purchase.payment_method, -@purchase.price_with_taxes, PURCHASE, @material.id, "مسح حركة بيع", 0)
     TreasuryDiary.where(transaction_id: @purchase.id, transaction_type: PURCHASE).destroy_all
     permissions.destroy_all
